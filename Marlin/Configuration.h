17ffe27061395c1c3ea65b6d10e0fde84e9b278c
Carl Marshall <zothommog42@gmail.com>
2021-11-20 11:13:37 -0600

    Updated MAX_TEMP
    Turns out MAX_TEMP isn't the highest you can set T0, it's the cut off time. I raised it to 290 and set the overshoot to 5. That'll let me set it to 285, but shut it off at 290.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -556,7 +556,7 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 285
+#define HEATER_0_MAXTEMP 290
 #define HEATER_1_MAXTEMP 285
 #define HEATER_2_MAXTEMP 285
 #define HEATER_3_MAXTEMP 285
@@ -573,7 +573,7 @@
  * (especially before PID tuning). Setting the target temperature too close to MAXTEMP guarantees
  * a MAXTEMP shutdown! Use these values to forbid temperatures being set too close to MAXTEMP.
  */
-#define HOTEND_OVERSHOOT 15   // (°C) Forbid temperatures over MAXTEMP - OVERSHOOT
+#define HOTEND_OVERSHOOT 5   // (°C) Forbid temperatures over MAXTEMP - OVERSHOOT
 #define BED_OVERSHOOT    10   // (°C) Forbid temperatures over MAXTEMP - OVERSHOOT
 #define COOLER_OVERSHOOT  2   // (°C) Forbid temperatures closer than OVERSHOOT
 
